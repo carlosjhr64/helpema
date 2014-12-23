a5634634759312ede05c63c9866acc8a8f9026a5
carlosjhr64 <carlosjhr64@gmail.com>
2014-12-23 13:57:24 -0800

    YouTubeDL

--- lib/helpema.rb
+++ lib/helpema.rb
@@ -1,11 +1,13 @@
 # Standard Libraries
 require 'open3'
 require 'timeout'
+require 'json'
 
 # This Gem
 require 'helpema/version.rb'
 require 'helpema/ssss.rb'
 require 'helpema/zbar.rb'
+require 'helpema/youtubedl.rb'
 
 # Requires:
 #`ruby`
