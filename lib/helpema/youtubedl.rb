da08cdfae470c3927fe9ee545bac5a9f47f23fc2
carlosjhr64 <carlosjhr64@gmail.com>
2021-01-06 06:21:39 -0800

    def YouTubdDL#json and ZBar#cam,#img

--- lib/helpema/youtubedl.rb
+++ lib/helpema/youtubedl.rb
@@ -6,9 +6,8 @@ class << self
   attr_accessor :version
   YouTubeDL.version = '^202\d\.[01]\d\.[0123]\d$'
 
-  # def json(url)
-  # @type method Helpema::YouTubeDL.json: (url: String url) { (untyped) -> untyped } -> untyped
-  define_command(:json,
+  def json(url, &blk) = _json(url:url, &blk)
+  define_command(:_json,
     cmd: 'youtube-dl', version: YouTubeDL.version,
     usage: {j:true, arg0:nil}, synonyms: {url: :arg0},
     err: '/dev/null'
--- lib/helpema/zbar.rb
+++ lib/helpema/zbar.rb
@@ -7,9 +7,8 @@ class << self
   ZBar.version = '^0\.2[345]$' # version as of this writing is 0.23
   ZBar.screenshot = ['gnome-screenshot', '-f']
 
-  # def cam()
-  # @type method Helpema::ZBar.cam: () -> String?
-  define_command(:cam,
+  def cam() = _cam()
+  define_command(:_cam,
     cmd: 'zbarcam', version: ZBar.version,
     usage: {raw:true,quiet:true,nodisplay:true, :prescale= => '800x800'},
     synonyms: {prescale: :prescale=}
@@ -21,16 +20,15 @@ class << self
     end
   end
 
-  # def img(filename:)
-  # @type method Helpema::ZBar.img: (filename: String filename) -> String?
-  define_command(:img, cmd: 'zbarimg', version: ZBar.version,
+  def img(filename) = _img(filename:filename)
+  define_command(:_img, cmd: 'zbarimg', version: ZBar.version,
                  usage: {q:true,raw:true,arg0:nil}, synonyms: {filename: :arg0})
 
   def screen
     raw = nil
     Dir.mktmpdir do |tmpdir|
      _ = File.join(tmpdir, "#{$$}.#{Time.now.to_f}.png")
-     raw = _ if snapshot(_) and not (_=ZBar.img(filename:_).chomp).empty?
+     raw = _ if snapshot(_) and not (_=ZBar.img(_).chomp).empty?
     end
     raw
   end
