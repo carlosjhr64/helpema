1b3abb0f6d0326c6da30429b7c50fd385a89c624
carlosjhr64 <carlosjhr64@gmail.com>
2021-07-06 05:38:32 -0700

    ZBar bug fix

--- lib/helpema/zbar.rb
+++ lib/helpema/zbar.rb
@@ -1,42 +1,46 @@
 require 'tmpdir'
 
 module Helpema
-module ZBar
-class << self
-  attr_accessor :version, :screenshot
-  ZBar.version = '^0\.2[345]\b' # version as of this writing is 0.23
-  ZBar.screenshot = ['gnome-screenshot', '-f']
+  module ZBar
+    extend Helpema
+    class << self; attr_accessor :version, :screenshot; end
+    ZBar.version = '^0\.2[345]\b' # version as of this writing is 0.23
+    ZBar.screenshot = ['gnome-screenshot', '-f']
 
-  def cam() = _cam()
-  define_command(:_cam,
-    cmd: 'zbarcam', version: ZBar.version,
-    usage: {raw:true,quiet:true,nodisplay:true, :prescale= => '800x800'},
-    synonyms: {prescale: :prescale=}
-  ) do |pipe, kw, blk|
-    begin
-      pipe.gets.chomp # This is the return value!
-    ensure
-      Process.kill('TERM', pipe.pid)
+    def snapshot(filename)
+      system(*ZBar.screenshot, filename)
     end
-  end
 
-  def img(filename) = _img(filename:filename)
-  define_command(:_img, cmd: 'zbarimg', version: ZBar.version,
-                 usage: {q:true,raw:true,arg0:nil}, synonyms: {filename: :arg0})
-
-  def screen
-    raw = nil
-    Dir.mktmpdir do |tmpdir|
-     _ = File.join(tmpdir, "#{$$}.#{Time.now.to_f}.png")
-     raw = _ if snapshot(_) and not (_=ZBar.img(_).chomp).empty?
+    ZBar.define_command(:_cam,
+                        cmd: 'zbarcam',
+                        version: ZBar.version,
+                        usage: {raw:true,quiet:true,nodisplay:true, :prescale= => '800x800'},
+                        synonyms: {prescale: :prescale=}
+    ) do |pipe, kw, blk|
+      begin
+        pipe.gets.chomp # This is the return value!
+      ensure
+        Process.kill('TERM', pipe.pid)
+      end
     end
-    raw
-  end
+    def cam() = ZBar.cam()
 
-  private
-  def snapshot(filename)
-    system(*ZBar.screenshot, filename)
+    ZBar.define_command(:_img,
+                        cmd: 'zbarimg',
+                        version: ZBar.version,
+                        usage: {q:true,raw:true,arg0:nil},
+                        synonyms: {filename: :arg0})
+    def img(filename) = ZBar._img(filename:filename)
+
+    def screen
+      raw = nil
+      Dir.mktmpdir do |tmpdir|
+       _ = File.join(tmpdir, "#{$$}.#{Time.now.to_f}.png")
+       raw = _ if ZBar.snapshot(_) and not (_=ZBar.img(_).chomp).empty?
+      end
+      raw
+    end
+
+    extend self
   end
-end
-end
 end
