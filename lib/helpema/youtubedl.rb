028c48b89a06ea90f7ac788fb282fcbf0c167204
carlosjhr64 <carlosjhr64@gmail.com>
2021-07-06 05:44:48 -0700

    YouTubeDL bug fixes

--- lib/helpema/youtubedl.rb
+++ lib/helpema/youtubedl.rb
@@ -1,25 +1,26 @@
 require 'json'
 
 module Helpema
-module YouTubeDL
-class << self
-  attr_accessor :version
-  YouTubeDL.version = '^202\d\.[01]\d\.[0123]\d$'
+  module YouTubeDL
+    extend Helpema
+    class << self; attr_accessor :version; end
+    YouTubeDL.version = '^202\d\.[01]\d\.[0123]\d$'
 
-  def json(url, &blk) = _json(url:url, &blk)
-  define_command(:_json,
-    cmd: 'youtube-dl', version: YouTubeDL.version,
-    usage: {j:true, arg0:nil}, synonyms: {url: :arg0},
-    err: '/dev/null'
-  ) do |pipe, options, blk|
-    pipe.each do |data|
-      begin
-        data = JSON.parse data
-      ensure
-        blk.call data
+    YouTubeDL.define_command(:_json,
+      cmd: 'youtube-dl', version: YouTubeDL.version,
+      usage: {j:true, arg0:nil}, synonyms: {url: :arg0},
+      err: '/dev/null'
+    ) do |pipe, options, blk|
+      pipe.each do |data|
+        begin
+          data = JSON.parse data
+        ensure
+          blk.call data
+        end
       end
     end
+    def json(url, &blk) = _json(url:url, &blk)
+
+    extend self
   end
-end
-end
 end
