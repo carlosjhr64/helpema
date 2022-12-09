06ea96af62ac4a4c6ccb11a36f547494fb5eca92
CarlosJHR64 <carlosjhr64@gmail.com>
2022-12-09 13:15:55 -0800

    Rubish review

--- lib/helpema/rubish.rb
+++ lib/helpema/rubish.rb
@@ -4,13 +4,15 @@ module Helpema
     def Rubish.shell(name,
                      cmd:name.to_s.chomp('?').chomp('!'),
                      version:nil, v:nil,
-                     usage:nil,   synonyms:nil)
+                     usage:nil,   synonyms:nil,
+                     default:nil)
       mode,exception = (name[-1]=='?')? ['w',false] : ['w+',nil]
       Rubish.define_command(name.to_sym,
                             cmd:cmd,
                             version:version, v:v,
                             usage:usage,     synonyms:synonyms,
-                            mode:mode,       exception:exception)
+                            mode:mode,       exception:exception,
+                            default:default) # default script
     end
     Rubish.shell :fish
     Rubish.shell :fish?
